190707dffd6e77248672be9111bc506a4677938f
John OKeeffe <john.ok.esp@gmail.com>
2022-05-30 21:05:09 -0500

    Added python version of palindrome including challenge spec

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,16 @@
 def palindrome(word):
-    # Write code here
+    validChars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    validStr = ""
+
+    for char in str(word).lower():
+        if char in validChars:
+            validStr += char
+    
+    midpoint = int(len(validStr) / 2)
+
+    word_front_half = validStr[:midpoint]
+    word_back_half_reversed = validStr[-1:-(midpoint) - 1:-1]
+
+    if word_front_half == word_back_half_reversed:
+        return True
+    return False
